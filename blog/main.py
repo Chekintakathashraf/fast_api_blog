f5e6e716d237ce468ee3906cbb36cf91622746c4
maasha <ashrafchekintakath@gmail.com>
2022-11-24 20:54:05 +0530

    crud operation

--- blog/main.py
+++ blog/main.py
@@ -40,3 +40,24 @@ def show(id, response: Response, db: Session = Depends (get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'detail' : f"Blog with this {id} not exist"}
     return blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends (get_db)):
+    blog =db.query(models.Blog).filter(models.Blog.id ==
+            id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends (get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Blog with id {id} not found")
+    blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'updated'
